646e085015f1f5850c3391ae56620b5819bad37b
Samantha Beaulieu <jsbeaulieu795@gmail.com>
2026-07-08 16:00:08 -0400

    Complete Phase 1.2: Initialize Spring Boot framework and MVC core

---
+++ src/test/java/com/portfolio/taskorganizer/TaskorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.taskorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
